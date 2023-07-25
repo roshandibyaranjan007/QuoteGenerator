c415ced284e7a379115eb880f44b22e413fa9347
roshandibyaranjan007 <139206878+roshandibyaranjan007@users.noreply.github.com>
2023-07-25 14:19:46 +0530

    Github js launchpad
    add files uploaded for quote generator

---
+++ index.js
@@ -0,0 +1,14 @@
+import { generateTextAndImage } from "./utils.js"
+
+let name = "Roshan Dibya Ranjan"
+
+let favoriteActivity = "Traveling"
+
+let favoritePlace = "Mountains"
+
+let temperature = 1.8
+
+// Optional: delete "avatar.jpg" and add a photo of yourself
+// (remember to use "avatar.jpg" as the name of your photo)
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
